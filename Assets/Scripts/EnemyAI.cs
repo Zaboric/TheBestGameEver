b1c115d5a62069df8c88a90fe025f1087ae12439
Zaboric <158718635+Zaboric@users.noreply.github.com>
2024-03-01 12:39:03 +0300

    Initial commit 4

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public List<Transform> patrolPoints;
+
+    private NavMeshAgent _navMeshAgent;
+
+    public PlayerController player;
+
+    private bool _isPlayerNoticed;
+
+    public float viewAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        NoticePlayerUpdate();
+
+        ChaseUpdate();
+
+        PatrolUpdate();  
+    }
+
+    private void PickNewPatrolPoint()
+    {      
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;             
+    }
+
+    private void PatrolUpdate()
+    {
+        if (_navMeshAgent.remainingDistance == 0)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+
+        if (Vector3.Angle(transform.forward, direction) < viewAngle)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+                else
+                {
+                    _isPlayerNoticed = false;
+                }
+            }
+        }
+    }
+
+    private void ChaseUpdate()
+    {
+        if (_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = player.transform.position;
+        }
+    }
+}
